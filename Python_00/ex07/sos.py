971f0d49b605e7a51d8891438a80f58309d67c7a
morajaon <morajaon@student.42.fr>
2026-05-14 10:56:47 +0200

    finished ex07

--- Python_00/ex07/sos.py
+++ Python_00/ex07/sos.py
@@ -17,14 +17,22 @@ NESTED_MORSE = {' ': "\\ ", 'A': '.- ', 'B': '-... ',
 
 
 def main():
+    """
+    Takes in arguement an alphanumeric string, translates and
+    outputs it into morse code on the standard output.
+    """
     args = sys.argv
     try:
         assert len(args) == 2
         assert isinstance(args[1], str)
         for c in args[1]:
-            assert c.isalnum()
+            assert c.isalnum() or c.isspace()
     except AssertionError:
         print("AssertionError: the arguments are bad")
+        sys.exit()
+    coded = [NESTED_MORSE.get(c.capitalize()) for c in args[1]]
+    print(str.join('', coded))
+
 
 if __name__ == "__main__":
     main()
