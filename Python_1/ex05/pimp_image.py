b39e818af2fbc92a9389b045a5c197e656b660f6
morajaon <morajaon@student.42.fr>
2026-07-15 11:00:06 +0200

    finished ex05 and Python_1

--- Python_1/ex05/pimp_image.py
+++ Python_1/ex05/pimp_image.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def display(array):
     '''displays the array as an image'''
-    plt.imshow(array)
+    plt.imshow(array, 'gray')
     try:
         plt.show()
     except KeyboardInterrupt:
@@ -31,7 +31,24 @@ def ft_green(array) -> np.array:  # type: ignore
     z = np.zeros((array.shape[0], array.shape[1], 1), dtype=np.uint8)
     r = np.array(array[:, :, 0:1])
     b = np.array(array[:, :, 2:3])
-    tmp = np.concatenate((r,z,b), 2)
+    tmp = np.concatenate((r, z, b), 2)
     res = array - tmp
     display(res)
-    return(res)
+    return res
+
+
+def ft_blue(array) -> np.array:  # type: ignore
+    '''Applies a blue color filter to the image provided'''
+    z = np.zeros((array.shape[0], array.shape[1], 1), dtype=np.uint8)
+    b = np.array(array[:, :, 2:3])
+    res = np.concatenate((z, z, b), 2)
+    display(res)
+    return res
+
+
+def ft_grey(array) -> np.array:  # type: ignore
+    '''Applies a greyscale filter to the image provided'''
+    res = np.sum(array, axis=2, dtype=np.uint16, keepdims=True)
+    res = np.uint8(res / 3)
+    display(res)
+    return res
