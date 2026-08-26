cab00301108942c5642a8dcc421825f819564a13
morajaon <morajaon@student.42.fr>
2026-08-26 16:11:57 +0200

    Finished ex00

--- Python_2/ex00/load_csv.py
+++ Python_2/ex00/load_csv.py
@@ -1,14 +1,20 @@
 import pandas as pd
 
+
 def error_exit(msg: str):
     '''prints an error msg and exits the program'''
     print(f"Error: {msg}")
     exit()
 
+
 def load(path: str) -> pd.DataFrame:
+    '''returns a DataFrame from a path to .csv file'''
     try:
         assert path != "", "empty path name"
+        assert path.find(".csv") > 0, "bad format"
         res = pd.read_csv(path)
+    except FileNotFoundError:
+        error_exit("File not foud")
     except AssertionError as e:
         error_exit(e)
     print(f"Loading dataset of dimensions {res.shape}")
--- Python_2/ex00/tester.py
+++ Python_2/ex00/tester.py
@@ -2,3 +2,5 @@ from load_csv import load
 
 
 print(load("life_expectancy_years.csv"))
+# print(load("test.txt"))
+# print(load("no_exist.csv"))
